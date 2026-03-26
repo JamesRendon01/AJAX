c5d99621e587272e0b3567f82aae47d0254e899c
James Rendon <rendondavid328@gmail.com>
2026-03-26 17:45:43 -0500

    feat: Se agrega componente FRONTEND

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h2>Lorem ipsum dolor sit amet consectetur.</h2>
+    </div>
+  )
+}
+
+export default App
